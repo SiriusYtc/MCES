8583bf27f87a9a8e1af4f19faab42d384457d75a
SiriusYtc <37736539+SiriusYtc@users.noreply.github.com>
2020-03-11 16:50:14 +0800

    update delete nuts

--- example_Bayesian_logistic_regression.m
+++ example_Bayesian_logistic_regression.m
@@ -9,21 +9,16 @@ X = [ones(n,1), X];
 f = @(beta) gradLogitPosterior(beta, y, X);
 theta0 = zeros(d+1, 1);
 
-%% Sample from the posterior with NUTS and plot ESS's
+
 n_warmup = 2000;
 n_mcmc_samples = 10000;
 n_re = 100; 
 
-theta_nuts = cell(1, n_re);
 theta_mces = cell(1, n_re);
-L_nuts = cell(1, n_re);
 L_mces = cell(1, n_re);
-ESS_NUTS = zeros(d+1, n_re);
 ESS_MCES = zeros(d+1, n_re);
 sample_use = 1 : 2 : n_mcmc_samples;
 for i = 1 : n_re
     [theta_mces{i}, L_mces{i}] = MCES_acce(f, theta0, n_warmup, n_mcmc_samples);
-    [theta_nuts{i}, L_nuts{i}] = NUTS_wrapper_c(f, theta0, n_warmup, n_mcmc_samples);
-    ESS_NUTS(:,i) = ESS(theta_nuts{i}(:, sample_use));
     ESS_MCES(:,i) = ESS(theta_mces{i}(:, sample_use));
 end
--- example_rosenbrock.m
+++ example_rosenbrock.m
@@ -1,27 +1,22 @@
 %% Load the data set and define a gradient function for the posterior of a 
 %  Rosenbrock. 
-a = 100;
-b = 0.2; 
+a = 1;
+b = 0; 
 
 %  Initialize Rosenbrock model. 
 f = @(theta)gradrbPosterior(theta, a, b);
 theta0 = zeros(2,1);
 
-%% Sample from the posterior with NUTS and plot ESS's
 n_warmup = 2000;
 n_mcmc_samples = 10000;
 n_re = 100;
 
-theta_nuts = cell(1, n_re);
+
 theta_mces = cell(1, n_re);
-L_nuts = cell(1, n_re);
 L_mces = cell(1, n_re);
-ESS_NUTS = zeros(d+1, n_re);
-ESS_MCES = zeros(d+1, n_re);
+ESS_MCES = zeros(2, n_re);
 sample_use = 1 : 2 : n_mcmc_samples;
 for i = 1 : n_re
     [theta_mces{i}, L_mces{i}] = MCES_acce(f, theta0, n_warmup, n_mcmc_samples);
-    [theta_nuts{i}, L_nuts{i}] = NUTS_wrapper_c(f, theta0, n_warmup, n_mcmc_samples);
-    ESS_NUTS(:,i) = ESS(theta_nuts{i}(:, sample_use));
     ESS_MCES(:,i) = ESS(theta_mces{i}(:, sample_use));
 end
